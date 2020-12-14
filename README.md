19cc8302c9bfe0c967ff95ad481623138f516bf3
sam-kh <khalfaouisamira@gmail.com>
2020-12-14 09:37:39 +0100

    TP final

---
+++ README.md
@@ -0,0 +1,7 @@
+"# WS-TP1-wcf-windowsForm : 
+    Procedure pas à pas  : Création et utilisation des Service WCF"
+
+#Ce travail vise à créer un service web de type Windows Communication Foundation (WCF) 
+#-ayant pour but d’effectuer des simples opérations d’arithmétiques (+ - * /) - 
+#avec IDE Visual Studio 2019 et le tester, 
+#puis le faire consommer à partir d’un client Application Windows Forms. 
---
+++ WindowsApplication1/WindowsApplication1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form
+    {
+        ServiceReferenceCalcul.ServiceClient cal = new ServiceReferenceCalcul.ServiceClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //addition
+        private void button1_Click(object sender, EventArgs e)
+        {
+           textBox3.Text = Convert.ToString(cal.WebAdd(int.Parse(textBox1.Text),int.Parse(textBox2.Text)));
+        }
+        //soustraction
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(cal.WebSoustraction(int.Parse(textBox1.Text), int.Parse(textBox2.Text)));
+        }
+        //Multiplication
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(cal.WebMultiply(int.Parse(textBox1.Text), int.Parse(textBox2.Text)));
+        }
+        //division
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(cal.WebDivision(int.Parse(textBox1.Text), int.Parse(textBox2.Text)));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
